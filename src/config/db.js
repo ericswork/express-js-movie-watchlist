a054b8461c2b60aa537495d349f8487596933c14
ericswork <ericcayobit.work@gmail.com>
2026-01-11 16:29:48 +0800

    Refactored DB handler

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  log:
-    process.env.NODE_ENV === "development"
-      ? ["query", "error", "warn"]
-      : ["error"],
+export const prisma = new PrismaClient({
+  log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
 });
 
-const connectDB = async () => {
+export const connectDB = async () => {
   try {
     await prisma.$connect();
     console.log("DB connected via Prisma.");
   } catch (error) {
     console.error(`Database connection error: ${error}`);
-    process.exit(1);
+    throw error;
   }
 };
 
-const disconnectDB = async () => {
-  await prisma.$disconnect();
+export const disconnectDB = async () => {
+  try {
+    await prisma.$disconnect();
+  } catch (error) {
+    console.error(`Database connection error: ${error}`);
+  }
 };
-
-export { connectDB, disconnectDB, prisma };
